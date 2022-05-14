6acd4bb3253608a9841f3c6e42affc0fc80bba55
StepanKhymera <strpankhymera@gmail.com>
2022-05-14 19:08:33 +0300

    added view model for song

---
+++ MusicRCM/Models/SongViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRCM.Models
+{
+    public class SongViewModel
+    {
+        public SongViewModel()
+        {
+        }
+        public string SearchQuery { get; set; }
+
+        public int PlaylistId { get; set; }
+        public int SpotifyId { get; set; }
+        public string SongName { get; set; }
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+
+       
+    }
+}
